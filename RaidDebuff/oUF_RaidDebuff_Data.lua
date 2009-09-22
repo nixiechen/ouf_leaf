b7b6410c6997f634c8a55cdc2a9fe79b6c989aef
Toni <yaroot@gmail.com>
2009-09-22 11:57:30 +0800

    update api

--- RaidDebuff/oUF_RaidDebuff_Data.lua
+++ RaidDebuff/oUF_RaidDebuff_Data.lua
@@ -14,6 +14,7 @@ local L = loc == 'zhCN' and {
 	['The Eye of Eternity'] = '永恒之眼',
 	['Ulduar'] = '奥杜尔',
 	['Trial of the Crusader'] = '十字军试炼',
+	['Vault of Archavon'] = '阿尔卡冯的宝库',
 } or loc == 'zhTW' and {
 	['Karazhan'] = '卡拉贊',
 	['Zul\'Aman'] = '祖阿曼',
@@ -27,75 +28,39 @@ local L = loc == 'zhCN' and {
 	['The Eye of Eternity'] = '永恆之眼',
 	['Ulduar'] = '奧杜亞',
 	['Trial of the Crusader'] = '十字軍試煉',
+	['Vault of Archavon'] = '亞夏梵穹殿',
 } or {}
 
 oUF_RaidDebuff.DebuffData = {}
 
 GridStatusRaidDebuff = {}
 
-GridStatusRaidDebuff.BZ = _G.BZ
-GridStatusRaidDebuff.BZ = _G.BB
-
-
-_G.BZ = setmetatable({}, {
-	__index = function(t,i)
-		return L[i] or i
-	end
-})
-
-_G.BB = setmetatable({}, {
-	__index = function(t,i)
-		return i
-	end
-})
-
-
-function GridStatusRaidDebuff:RegisterDebuff(zone, debuffID, order, duration, stackable, color, disable, auraCheck)
-	local dd = oUF_RaidDebuff.DebuffData
-	dd[zone] = dd[zone] or {}
-	local zone_table = dd[zone]
+function GridStatusRaidDebuff:Debuff(en_zone, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon)
+	local ddata = oUF_RaidDebuff.DebuffData
+	local zone = L[en_zone] or en_zone
+	ddata[zone] = ddata[zone] or {}
+	local zone_table = ddata[zone]
 	zone_table[debuffID] = {
 		['order'] = order,
-		['duration'] = duration,
+		['timer'] = timer,
 		['stackable'] = stackable,
 		['color'] = color,
-		--['disable'] = disable,
-		['auraCheck'] = auraCheck,
 	}
 end
 
-function GridStatusRaidDebuff:RegisterDebuffDelegater(zone, debuffID, ...)
-	local dd = oUF_RaidDebuff.DebuffData
-	local zone_table = dd[zone]
-
-	local n = select('#',...)
-	if n > 0 then
-		for i = 1,n do
-			local delegaterID = select(i,...)
-			zone_table[delegaterID] = CopyTable(zone_table[debuffID])
-		end
-	end
-
-	
+function GridStatusRaidDebuff:BossName(en_zone, order, en_boss)
 end
-
-function GridStatusRaidDebuff:RegisterMenuHeader(zone, order, header)
-end
-function GridStatusRaidDebuff:RegisterDebuffDelMod(zone, delID, debuffID, duration, stackable, color)
-end
-
 
 function GridStatusRaidDebuff:Wipe()
-	_G.BZ = GridStatusRaidDebuff.BZ
-	_G.BB = GridStatusRaidDebuff.BZ
+	wipe(GridStatusRaidDebuff)
+	GridStatusRaidDebuff = nil
 end
-
 
 if not ouf_leaf.test_mod then return end
 
-GridStatusRaidDebuff:RegisterDebuff('沙塔斯城', 33763, 1, 7, true)
-GridStatusRaidDebuff:RegisterDebuff('沙塔斯城', 33763, 1, 7, true)
+GridStatusRaidDebuff:Debuff('沙塔斯城', 33763, 1, 7, true)
+GridStatusRaidDebuff:Debuff('沙塔斯城', 33763, 1, 7, true)
 
-GridStatusRaidDebuff:RegisterDebuff('沙塔斯城', 26982, 2, 12)
-GridStatusRaidDebuff:RegisterDebuffDelegater('沙塔斯城', 26982, 26981)
+GridStatusRaidDebuff:Debuff('沙塔斯城', 26982, 2, 12)
+GridStatusRaidDebuff:Debuff('沙塔斯城', 26982, 26981)
 

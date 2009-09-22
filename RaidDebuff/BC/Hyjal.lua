36c35b7083ce420d48b12a519780f752921b027a
Toni <yaroot@gmail.com>
2009-09-22 12:32:26 +0800

    blah

--- RaidDebuff/BC/Hyjal.lua
+++ RaidDebuff/BC/Hyjal.lua
@@ -1,23 +1,19 @@
-﻿local zone = BZ["Hyjal Summit"]
+﻿local zone = "Hyjal Summit"
 
 --en_zone, debuffID, order, icon_priority, color_priority, timer, stackable, color, default_disable, noicon
 
 --Winterchill
-GridStatusRaidDebuff:RegisterMenuHeader(zone, 10, BB["Rage Winterchill"])
-GridStatusRaidDebuff:RegisterDebuff(zone, 31249, 11, 4) --Ice Bolt
+GridStatusRaidDebuff:Debuff(zone, 31249, 11, 5, 5, true) --Ice Bolt
 
 --Aneteron
-GridStatusRaidDebuff:RegisterMenuHeader(zone, 20, BB["Anetheron"])
-GridStatusRaidDebuff:RegisterDebuff(zone, 31306, 21, 20) --Carrion Swarm
-GridStatusRaidDebuff:RegisterDebuff(zone, 31298, 22, 10) --Sleep
+GridStatusRaidDebuff:Debuff(zone, 31306, 21, 5, 5, true) --Carrion Swarm
+GridStatusRaidDebuff:Debuff(zone, 31298, 22, 5, 5, true) --Sleep
 
 --Azgalor
-GridStatusRaidDebuff:RegisterMenuHeader(zone, 30, BB["Azgalor"])
-GridStatusRaidDebuff:RegisterDebuff(zone, 31347, 31, 20) --Doom
-GridStatusRaidDebuff:RegisterDebuff(zone, 31341, 32, 5) --Unquenchable Flames
-GridStatusRaidDebuff:RegisterDebuff(zone, 31344, 33, 5) --Howl of Azgalor
+GridStatusRaidDebuff:Debuff(zone, 31347, 31, 5, 5, true) --Doom
+GridStatusRaidDebuff:Debuff(zone, 31341, 32, 5, 5, true) --Unquenchable Flames
+GridStatusRaidDebuff:Debuff(zone, 31344, 33, 5, 5, true) --Howl of Azgalor
 
 --Achimonde
-GridStatusRaidDebuff:RegisterMenuHeader(zone, 40, BB["Archimonde"])
-GridStatusRaidDebuff:RegisterDebuff(zone, 31944, 41, 45) --Doomfire
-GridStatusRaidDebuff:RegisterDebuff(zone, 31972, 42) --Grip
+GridStatusRaidDebuff:Debuff(zone, 31944, 41, 5, 5, true) --Doomfire
+GridStatusRaidDebuff:Debuff(zone, 31972, 42, 5, 5) --Grip
